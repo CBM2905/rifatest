0d3af298e154a5f19c9d9f1185701ae7890be248
CBM <cbmillos29@gmail.com>
2025-10-10 08:48:14 -0500

    ya

--- config.js
+++ config.js
@@ -1,22 +1,12 @@
-// Carga las variables de entorno
-const loadEnvVariables = async () => {
-  try {
-    const response = await fetch('/.env');
-    const text = await response.text();
-    const vars = text.split('\n').reduce((acc, line) => {
-      const [key, value] = line.split('=');
-      if (key && value) {
-        acc[key.trim()] = value.trim();
-      }
-      return acc;
-    }, {});
-    
-    window.process = {
-      env: vars
-    };
-  } catch (error) {
-    console.error('Error loading environment variables:', error);
+// Configuración de Firebase
+window.process = {
+  env: {
+    FIREBASE_API_KEY: "AIzaSyBuvYBZUYfFcX8NPiHgYVQHlNb7rsEvtLs",
+    FIREBASE_AUTH_DOMAIN: "testt-432a7.firebaseapp.com",
+    FIREBASE_PROJECT_ID: "testt-432a7",
+    FIREBASE_STORAGE_BUCKET: "testt-432a7.firebasestorage.app",
+    FIREBASE_MESSAGING_SENDER_ID: "377883179539",
+    FIREBASE_APP_ID: "1:377883179539:web:8ecece01b14a8bd65a5de6",
+    ADMIN_PIN: "1234"
   }
 };
-
-loadEnvVariables();
